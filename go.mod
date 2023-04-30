25455f65877f5fa95a94f2e1c2fe2c425e36c1dc
Ryan Punt <ryan@mirum.org>
2023-04-30 08:39:24 -0500

    bump dependencies

--- go.mod
+++ go.mod
@@ -6,6 +6,6 @@ go 1.19
 
 // require github.com/rpunt/f1apireader v0.0.0-00010101000000-000000000000
 
-require github.com/rpunt/f1apireader v1.4.0
+require github.com/rpunt/f1apireader v1.4.1
 
-require github.com/rpunt/simplehttp v0.0.4 // indirect
+require github.com/rpunt/simplehttp v0.0.5 // indirect
--- hamilton-updater.go
+++ hamilton-updater.go
@@ -7,19 +7,10 @@ import (
 )
 
 func main() {
-	// branch = 'main'
-	// g = Git.open(__dir__)
-	// g.checkout(branch)
-	// # g.pull
-
 	race, err := f1apireader.RaceResults()
 	if err != nil {
 		fmt.Println("err:", err)
 	}
-
-	// config = YAML.load_file("#{__dir__}/_config.yml")
-	// config['description'] = "#{race_status['race']['meetingCountryName']}: #{raceStart.strftime('%d %B %Y')}"
-	// File.write("#{__dir__}/_config.yml", config.to_yaml)
 
 	raceStatus, err := race.Status()
 	if err != nil {
@@ -39,13 +30,4 @@ func main() {
 	} else {
 		fmt.Printf("race is %s\n", raceStatus)
 	}
-
-	// File.write("#{__dir__}/index.md", "# #{answer}")
-	// File.write("#{__dir__}/api_results/#{Time.now.strftime('%Y%m%d-%H%M%S')}.json", JSON.pretty_generate(race_status))
-
-	// if g.status.changed.count.positive?
-	// 	g.add(all: true)
-	// 	g.commit_all("updating for #{race_status['race']['meetingCountryName']}: #{raceStart.strftime('%d %B %Y')}; race #{raceinfo['state']}")
-	// 	g.push(g.remote('origin'), branch)
-	// end
 }
